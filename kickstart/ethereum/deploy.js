bf4ed6acbeff3cace9cb2c5c2572dec2c22b41ac
DenisRomanovsky <denys.r@stadiumgoods.com>
2021-08-08 15:02:26 +0300

    Campaing with tests

---
+++ kickstart/ethereum/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+const provider = new HDWalletProvider(
+    "YOUR MNEMONIC",
+    "https://rinkeby.infura.io/v3/b93bfc5f4dc9412bb5467ed7a55b8f1b"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+deploy();
+
+//0x6682b8355737C3C5F05B80076E131FA140FF1ea8 - the address
